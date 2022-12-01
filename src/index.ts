8bb1fd5c93a07ddcafb9600c834048d4b11f348f
juliasut <juliasutygina@gmail.com>
2022-12-01 11:52:22 -0700

    Create scrollable page with 3d animatin

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,256 @@
 import {
-    ViewerApp,
-    AssetManagerPlugin,
-    GBufferPlugin,
-    timeout,
-    ProgressivePlugin,
-    TonemapPlugin,
-    SSRPlugin,
-    SSAOPlugin,
-    DiamondPlugin,
-    FrameFadePlugin,
-    GLTFAnimationPlugin,
-    GroundPlugin,
-    BloomPlugin,
-    TemporalAAPlugin,
-    AnisotropyPlugin,
-    GammaCorrectionPlugin,
+  ViewerApp,
+  AssetManagerPlugin,
+  GBufferPlugin,
+  ProgressivePlugin,
+  TonemapPlugin,
+  SSRPlugin,
+  SSAOPlugin,
+  BloomPlugin,
+  Vector3,
+  GammaCorrectionPlugin,
+  MeshBasicMaterial2,
+  Color,
+} from 'webgi';
+import './styles.css';
 
-    addBasePlugins,
-    ITexture, TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-    IViewerPlugin,
+gsap.registerPlugin(ScrollTrigger);
 
-    // Color, // Import THREE.js internals
-    // Texture, // Import THREE.js internals
-} from "webgi";
-import "./styles.css";
+async function setupViewer() {
+  const viewer = new ViewerApp({
+    canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+    // isAntialiased: true,
+  });
 
-async function setupViewer(){
+  const manager = await viewer.addPlugin(AssetManagerPlugin);
+  const camera = viewer.scene.activeCamera;
+  const position = camera.position;
+  const target = camera.target;
+  const exitButton = document.querySelector('.button--exit') as HTMLElement;
+  const customizerInterface = document.querySelector(
+    '.customizer--container'
+  ) as HTMLElement;
 
-    // Initialize the viewer
-    const viewer = new ViewerApp({
-        canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+  // Add plugins individually.
+  await viewer.addPlugin(GBufferPlugin);
+  await viewer.addPlugin(new ProgressivePlugin(32));
+  await viewer.addPlugin(new TonemapPlugin(true));
+  await viewer.addPlugin(GammaCorrectionPlugin);
+  await viewer.addPlugin(SSRPlugin);
+  await viewer.addPlugin(SSAOPlugin);
+  await viewer.addPlugin(BloomPlugin);
+
+  viewer.renderer.refreshPipeline();
+
+  await manager.addFromPath('./assets/drill3.glb');
+
+  const drillMaterial = manager.materials!.findMaterialsByName(
+    'Drill_01'
+  )[0] as MeshBasicMaterial2;
+
+  viewer.getPlugin(TonemapPlugin)!.config!.clipBackground = true; // in case its set to false in the glb
+
+  viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
+
+  onUpdate();
+
+  function setupScrollanimation() {
+    const tl = gsap.timeline();
+
+    // FIRST SECTION
+
+    tl.to(position, {
+      x: 1.56,
+      y: -2.26,
+      z: -3.85,
+      scrollTrigger: {
+        trigger: '.second',
+        start: 'top bottom',
+        end: 'top top',
+        scrub: true,
+        immediateRender: false,
+      },
+      onUpdate,
     })
 
-    // Add some plugins
-    const manager = await viewer.addPlugin(AssetManagerPlugin)
+      .to('.section--one--container', {
+        xPercent: '-150',
+        opacity: 0,
+        scrollTrigger: {
+          trigger: '.second',
+          start: 'top bottom',
+          end: 'top 80%',
+          scrub: 1,
+          immediateRender: false,
+        },
+      })
+      .to(target, {
+        x: -1.37,
+        y: 1.99,
+        z: -0.37,
+        scrollTrigger: {
+          trigger: '.second',
+          start: 'top bottom',
+          end: 'top top',
+          scrub: true,
+          immediateRender: false,
+        },
+      })
 
-    // Add a popup(in HTML) with download progress when any asset is downloading.
-    await viewer.addPlugin(AssetManagerBasicPopupPlugin)
+      // LAST SECTION
 
-    // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(GammaCorrectionPlugin)
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
+      .to(position, {
+        x: -3.4,
+        y: 9.6,
+        z: 1.71,
+        scrollTrigger: {
+          trigger: '.third',
+          start: 'top bottom',
+          end: 'top top',
+          scrub: true,
+          immediateRender: false,
+        },
+        onUpdate,
+      })
 
-    // or use this to add all main ones at once.
-    await addBasePlugins(viewer)
+      .to(target, {
+        x: -1.5,
+        y: 2.13,
+        z: -0.4,
+        scrollTrigger: {
+          trigger: '.third',
+          start: 'top bottom',
+          end: 'top top',
+          scrub: true,
+          immediateRender: false,
+        },
+      });
+  }
 
-    // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-    await viewer.addPlugin(CanvasSnipperPlugin)
+  setupScrollanimation();
 
-    // This must be called once after all plugins are added.
-    viewer.renderer.refreshPipeline()
+  // WEBGI UPDATE
+  let needsUpdate = true;
 
-    await manager.addFromPath("./assets/classic-watch.glb")
+  function onUpdate() {
+    needsUpdate = true;
+    // viewer.renderer.resetShadows()
+    viewer.setDirty();
+  }
 
-    // Load an environment map if not set in the glb file
-    // await viewer.scene.setEnvironment(
-    //     await manager.importer!.importSinglePath<ITexture>(
-    //         "./assets/environment.hdr"
-    //     )
-    // );
+  viewer.addEventListener('preFrame', () => {
+    if (needsUpdate) {
+      camera.positionTargetUpdated(true);
+      needsUpdate = false;
+    }
+  });
 
-    // Add some UI for tweak and testing.
-    const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
-    // Add plugins to the UI to see their settings.
-    uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
+  // KNOW MORE EVENT
+  document.querySelector('.button--hero')?.addEventListener('click', () => {
+    const element = document.querySelector('.second');
+    window.scrollTo({
+      top: element?.getBoundingClientRect().top,
+      left: 0,
+      behavior: 'smooth',
+    });
+  });
 
+  // SCROLL TO TOP
+  document.querySelectorAll('.button--footer')?.forEach((item) => {
+    item.addEventListener('click', () => {
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    });
+  });
+
+  // CUSTOMIZE
+  const sections = document.querySelector('.container') as HTMLElement;
+  const mainContainer = document.getElementById(
+    'webgi-canvas-container'
+  ) as HTMLElement;
+  document
+    .querySelector('.button--customize')
+    ?.addEventListener('click', () => {
+      sections.style.visibility = 'hidden';
+      mainContainer.style.pointerEvents = 'all';
+      document.body.style.cursor = 'grab';
+
+      gsap.to(position, {
+        x: -2.6,
+        y: 0.2,
+        z: -9.6,
+        duration: 2,
+        ease: 'power3.inOut',
+        onUpdate,
+      });
+      gsap.to(target, {
+        x: -0.15,
+        y: 1.18,
+        z: 0.12,
+        duration: 2,
+        ease: 'power3.inOut',
+        onUpdate,
+        onComplete: enableControlers,
+      });
+    });
+
+  function enableControlers() {
+    exitButton.style.visibility = 'visible';
+    customizerInterface.style.visibility = 'visible';
+    viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: true });
+  }
+
+  // EXIT CUSTOMIZER
+  exitButton.addEventListener('click', () => {
+    gsap.to(position, {
+      x: -3.4,
+      y: 9.6,
+      z: 1.71,
+      duration: 1,
+      ease: 'power3.inOut',
+      onUpdate,
+    });
+    gsap.to(target, {
+      x: -1.5,
+      y: 2.13,
+      z: -0.4,
+      duration: 1,
+      ease: 'power3.inOut',
+      onUpdate,
+    });
+
+    viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
+    sections.style.visibility = 'visible';
+    mainContainer.style.pointerEvents = 'none';
+    document.body.style.cursor = 'default';
+    exitButton.style.visibility = 'hidden';
+    customizerInterface.style.visibility = 'hidden';
+  });
+
+  document
+    .querySelector('.button--colors.black')
+    ?.addEventListener('click', () => {
+      changeColor(new Color(0x383830).convertSRGBToLinear());
+    });
+
+  document
+    .querySelector('.button--colors.red')
+    ?.addEventListener('click', () => {
+      changeColor(new Color(0xfe2d2d).convertSRGBToLinear());
+    });
+
+  document
+    .querySelector('.button--colors.yellow')
+    ?.addEventListener('click', () => {
+      changeColor(new Color(0xffffff).convertSRGBToLinear());
+    });
+
+  function changeColor(_colorToBeChanged: Color) {
+    drillMaterial.color = _colorToBeChanged;
+    viewer.scene.setDirty();
+  }
 }
 
-setupViewer()
+setupViewer();
